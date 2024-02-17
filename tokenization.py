f4adf7975c5f3d5c8d7891a72fdd963485e7e744
daze lu <0lu0da0ze0@gmail.com>
2024-02-16 17:17:29 -0800

    modified

--- tokenization.py
+++ tokenization.py
@@ -7,44 +7,47 @@ from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
 from transformers import PreTrainedTokenizerFast
 
-def count_word_the():
-    root = '/home/work/disk/language-data'
-    for target_dir in ['english_wikipedia']:
-        abdir_path = os.path.join(root, target_dir)
-        files = list(map(lambda x: os.path.join(abdir_path, x), os.listdir(abdir_path)))
-        files = list(filter(lambda x: 'train' in x, files))
-        files.sort()
-        knt = 0
-        for file in files:
-            with open(file, 'r') as f:
-                lines = f.readlines()
-                lines = list(map(lambda x: len(x.split('alaal'))-1, lines))
-                knt += sum(lines)
-            break
-        print(target_dir, knt)
+# def count_word_the():
+#     root = '/home/work/disk/language-data'
+#     for target_dir in ['english_wikipedia']:
+#         abdir_path = os.path.join(root, target_dir)
+#         files = list(map(lambda x: os.path.join(abdir_path, x), os.listdir(abdir_path)))
+#         files = list(filter(lambda x: 'train' in x, files))
+#         files.sort()
+#         knt = 0
+#         for file in files:
+#             with open(file, 'r') as f:
+#                 lines = f.readlines()
+#                 lines = list(map(lambda x: len(x.split('alaal'))-1, lines))
+#                 knt += sum(lines)
+#             break
+#         print(target_dir, knt)
+#
+# def get_all_files(root, target_dirs, ratio=0.3):
+#     result = []
+#     for dir in target_dirs:
+#         abdir_path = os.path.join(root, dir)
+#         files = list(map(lambda x: os.path.join(abdir_path, x), os.listdir(abdir_path)))
+#         files = list(filter(lambda x: 'train' in x, files))
+#         files.sort()
+#         result.extend(files[:int(len(files)*ratio)])
+#     return result
 
-def get_all_files(root, target_dirs, ratio=0.3):
-    result = []
-    for dir in target_dirs:
-        abdir_path = os.path.join(root, dir)
-        files = list(map(lambda x: os.path.join(abdir_path, x), os.listdir(abdir_path)))
-        files = list(filter(lambda x: 'train' in x, files))
-        files.sort()
-        result.extend(files[:int(len(files)*ratio)])
-    return result
-
-def bpe_tokenization(model_root, data_root):
+# def bpe_tokenization(model_root, data_root):
+def bpe_tokenization():
     tokenizer = Tokenizer(BPE(unk_token="<unk>", fuse_unk=True))
     tokenizer.normalizer = normalizers.Sequence([
-        normalizers.NFKC()
+        normalizers.NFKC(),
+        normalizers.Replace(' ', '_')
     ]) 
     tokenizer.pre_tokenizer = pre_tokenizers.Sequence([
-        pre_tokenizers.Split(Regex(' '), behavior='merged_with_next'),
-        pre_tokenizers.Split(Regex('\d|[\u2E80-\u2FDF\u3040-\u318F\u31A0-\u31BF\u31F0-\u31FF\u3400-\u4DB5\u4E00-\u9FFF\uA960-\uA97F\uAC00-\uD7FF]'), behavior='isolated'),
-        pre_tokenizers.Split(Regex(' *(\w+|[^\w\s]+)'), behavior='isolated'),
+        pre_tokenizers.Split(Regex('_'), behavior='merged_with_next'),
+        pre_tokenizers.Split(Regex('\d'), behavior='isolated'),
+        pre_tokenizers.Split(Regex('_*(\w+|[^\w\s]+)'), behavior='isolated'),
         pre_tokenizers.ByteLevel(add_prefix_space=False, use_regex=False)
     ])
     tokenizer.decoder = decoders.Sequence([
+        decoders.Replace('_', ' '),
         decoders.ByteLevel(),
     ])
     trainer = BpeTrainer(special_tokens=["<unk>", "<s>", "</s>"], 
@@ -54,12 +57,12 @@ def bpe_tokenization(model_root, data_root):
     # tokenizer.train_from_iterator(text_iterator(), trainer=trainer) # memory leak
     # tokenizer.train(files=['/home/work/disk/vision/retriever/text.txt'], trainer=trainer)
 
-    target_dirs = ['english_gutenberg', 'english_wikipedia', 'english_books3', 'english_c4'] #'english_gutenberg', 'english_wikipedia', 'english_books3', 'english_c4'
-    train_files = get_all_files(data_root, target_dirs, ratio=0.35)
-    tokenizer.train(files=train_files, trainer=trainer) # 325242 Mo, 69323261 pairs
+    # target_dirs = ['english_gutenberg', 'english_wikipedia', 'english_books3', 'english_c4'] #'english_gutenberg', 'english_wikipedia', 'english_books3', 'english_c4'
+    # train_files = get_all_files(data_root, target_dirs, ratio=0.35)
+    tokenizer.train(files=['./data/train.txt', './data/validation.txt'], trainer=trainer) # 325242 Mo, 69323261 pairs
 
-    save_path = os.path.join(model_root, 'tokenizer.json')
-    tokenizer.save(save_path)
+    # save_path = os.path.join(model_root, 'tokenizer.json')
+    tokenizer.save('./tokenizer_daze/tokenizer.json')
     output = tokenizer.encode("Hello, y'all! How are you 😁您 Ã?\nwhat is the meal today")
     print(output.tokens)
     output = tokenizer.decode(output.ids)
@@ -101,11 +104,11 @@ def bpe_validate(model_root, model_dir, model_name, data_root):
     # 训练数据868G，200B tokens
 
 if __name__ == "__main__":
-    model_root = "/home/work/disk/vision/retriever"
-    model_dir = "tokenizer_models"
-    model_name="tokenizer_300G.json"
-    # model_name="tokenizer_u32.json"
-    data_root = '/home/work/disk/language-data'
+    # model_root = "/home/work/disk/vision/retriever"
+    # model_dir = "tokenizer_models"
+    # model_name="tokenizer_300G.json"
+    # # model_name="tokenizer_u32.json"
+    # data_root = '/home/work/disk/language-data'
 
     # hugging face的BPE训练出来有问题，一些常用词合成不出来，如"the"->"t","h","e"
     # 查了下，应该是overflow的问题
@@ -122,4 +125,5 @@ if __name__ == "__main__":
     # bpe_tokenization(model_root, data_root)
 
     # 验证模型是否有问题
-    bpe_validate(model_root, model_dir, model_name, data_root)
+    # bpe_validate(model_root, model_dir, model_name, data_root)
+    bpe_tokenization()
